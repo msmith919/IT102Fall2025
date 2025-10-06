98b8ef39375857bfb36b74281df9b2d5c75c6bc0
msmith919 <smith.margaret@student.greenriver.edu>
2025-10-05 19:15:37 -0700

    Added prompt for user name & positive affirmation

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
-# Created 
+# Created by Margaret
+
+#Prompts the user for their name
+name = input("Please enter your name:")
+
+positive_affirmation = "This quarter is going to be the best!"
+#Print hello name?
+print(name)
+
+#Print a positive affirmation
+print(positive_affirmation)
