f03644b70117d732616269168ea4aba58775df45
msmith919 <smith.margaret@student.greenriver.edu>
2025-10-12 19:14:15 -0700

    10 times loop

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 # example workign with Loops
-#By 
+#By Margaret
+
+question = input("Is today a good day? Please answer with y or n:")
+
+if question == 'y':
+    #Create a loop of 10 times saying yea it is"
+    number = 1 
+    while number < 11:
+        print("yes it is")
+        number += 1
+elif question == 'n':
+    print('Thats okay, tomorrow will be ')
+elif question == 'idk':
+    print('thats okay')
+else:
+    print('Please enter a valid input pf y or n for yes or no')
